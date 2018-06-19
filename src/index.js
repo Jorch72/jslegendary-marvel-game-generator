1e99a1b6dd0c2d01058d575ff6f7a0af41b51bcf
Seth Gunnells <sethgunnells@gmail.com>
2018-06-18 23:37:48 -0500

    Add basic work on setting initial state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import { h, app } from 'hyperapp';
+import { getInitialConfig } from './services/gen.js';
 
-const view = () => <h1>Hello, World!</h1>;
+const actions = {
+  getInitialConfig: getInitialConfig
+};
 
-app({}, {}, view, document.body);
+const view = (state, actions) => (<div>
+  <h1>LEGENDARY Game Generator</h1>
+  <div>
+    <button onclick={() => actions.getInitialConfig(2)}>Two</button>
+    <button onclick={() => actions.getInitialConfig(3)}>Three</button>
+    <button onclick={() => actions.getInitialConfig(4)}>Four</button>
+    <button onclick={() => actions.getInitialConfig(5)}>Five</button>
+  </div>
+</div>);
+
+app({ bystanders: 0 }, actions, view, document.body);
