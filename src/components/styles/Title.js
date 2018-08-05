1d2b278bc720920b7e914dca1afb86469a9c5c73
Seth Gunnells <sethgunnells@gmail.com>
2018-08-05 10:08:53 -0500

    Remove emotion from app

--- src/components/styles/Title.js
+++
@@ -1,8 +0,0 @@
-import { h } from 'hyperapp';
-import { css } from 'emotion';
-
-const style = css`
-  font-family: Helvetica, sans-serif;
-`;
-
-export default (props, children) => <h1 className={style}>{children}</h1>;
--- src/index.js
+++ src/index.js
@@ -5,30 +5,31 @@ import ExpansionList from './components/ExpansionList.js';
 // import GameDisplay from './components/GameDisplay.js';
 import PlayerButtons from './components/PlayerButtons.js';
 import Section from './components/Section.js';
-import H1 from './components/styles/Title.js';
 
 const data = loadData();
 
 const view = (state, actions) => {
   const goToSection2 = () => actions.setSection(2);
 
-  return <div>
-    <H1>LEGENDARY Game Generator</H1>
+  return (
+    <div>
+      <h1>LEGENDARY Game Generator</h1>
 
-    <Section current={state.section} section={0}>
-      <h2>How Many Players?</h2>
-      <PlayerButtons selectPlayers={actions.selectPlayers} />
-    </Section>
+      <Section current={state.section} section={0}>
+        <h2>How Many Players?</h2>
+        <PlayerButtons selectPlayers={actions.selectPlayers} />
+      </Section>
 
-    <Section current={state.section} section={1}>
-      <h2>Which Expansions Do You Want To Use?</h2>
-      <ExpansionList
-        toggle={actions.toggleExpansion}
-        expansions={state.expansions}
-      />
-      <button onclick={goToSection2}>Next</button>
-    </Section>
-  </div>;
+      <Section current={state.section} section={1}>
+        <h2>Which Expansions Do You Want To Use?</h2>
+        <ExpansionList
+          toggle={actions.toggleExpansion}
+          expansions={state.expansions}
+        />
+        <button onclick={goToSection2}>Next</button>
+      </Section>
+    </div>
+  );
 };
 
 app(data, actions, view, document.body);
